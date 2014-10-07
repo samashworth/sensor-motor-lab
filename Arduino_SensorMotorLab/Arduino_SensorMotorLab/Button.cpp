f3e9bfa3564b9b6524692d9164e83242b609b88c
rnasci <rnasci@andrew.cmu.edu>
2014-10-07 14:47:25 -0400

    Debugging Button
    Bug Chasing

--- Arduino_SensorMotorLab/Arduino_SensorMotorLab/Button.cpp
+++ Arduino_SensorMotorLab/Arduino_SensorMotorLab/Button.cpp
@@ -1,9 +1,12 @@
 #include "Button.h"
+#include "Messenger.h"
+
 
 void Button::initialize(int numPins, int pinIds[], int numInterrupts, int interruptIds[])
 {
   
   button = pinIds[0];
+  pinMode(button,INPUT);
   buttonState = LOW;
   lastButtonState = LOW;
   currentButtonState = HIGH;
@@ -15,6 +18,7 @@ boolean Button::pressOccurred()
 {
   currentButtonState = debounce(lastButtonState, button);
   if(currentButtonState == LOW && lastButtonState == HIGH){
+      Messenger::printMessage("Button Pressed", false);
       buttonState = HIGH;
       lastButtonState = currentButtonState;
     } 
